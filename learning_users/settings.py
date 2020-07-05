7d90f14cd8b77c2836210a121aa1de8388518fd9
Aman Shaw <amanshaw38@gmail.com>
2020-07-05 19:48:50 +0530

    Added Hosts

--- learning_users/settings.py
+++ learning_users/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'jqvwl3fecxgg+1bwwp5nla7dadwqt4uxbz&iajat)ni+=yjy4b'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['amanshaw.pythonanywhere.com', ]
 
 
 # Application definition
